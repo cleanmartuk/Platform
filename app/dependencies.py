31a7a5c94218921f688910c79defab3014f8783a
Spanarchian <spanarchian@gmail.com>
2021-07-12 16:31:04 +0100

    REFACTORED: Dependencies.py

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,18 +1,20 @@
 from fastapi import Header, HTTPException
+
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 
-evn_x_token = pword = os.getenv('x_token')
-evn_token = pword = os.getenv('token')
+evn_x_token = os.getenv('x_token')
+evn_token = os.getenv('token')
 
 
 async def get_token_header(x_token: str = Header(...)):
-    if x_token != evn_x_token:
+    print(f"evn_x_token = {evn_x_token}")
+    if x_token != "fake-super-secret-token":
         raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
 async def get_query_token(token: str):
-    if token != evn_token:
+    if token != "jessica":
         raise HTTPException(status_code=400, detail="No Jessica token provided")
--- app/gdb/data_models/client.py
+++ app/gdb/data_models/client.py
@@ -8,14 +8,27 @@ from py2neo import Graph
 graph = Graph("bolt://localhost:7687")
 
 
-class User(BaseModel):
-    id: str
-    name = 'John Doe'
-    houseNumbber = int
-    location = str
-    password = str
-    signup_ts: Optional[datetime] = None
+class Client(BaseModel):
+    id : str
+    status : bool
+    name : str
+    houseNumber : int
+    location : str
+    password : str
     JobSheet: List[ str ] = []
+    class Config:
+        '''Docstring here.'''
+        schema_extra = {
+            "example": {
+                "id": "7f644301-e3f1-4752-90d5-99fbfad91ab3",
+                "status" : True,
+                "name" : 'John Doe',
+                "houseNumber" : 23,
+                "location" : "DT1 1SS",
+                "password" : "Secret_Pa55w0rd",
+                "JobSheet": ["79dc3d3a-c40b-47e8-8cf4-207c2de7e36","c85a5633-2803-4826-ae5a-82474c238d5","0348ae36-202a-4bfc-a92d-849607fd541" ],
+            }
+        }
 
 external_data = {
     "id": "7f644301-e3f1-4752-90d5-99fbfad91ab4",
@@ -28,31 +41,6 @@ external_data = {
     "JobSheet": ["79dc3d3a-c40b-47e8-8cf4-207c2de7e36","c85a5633-2803-4826-ae5a-82474c238d5","0348ae36-202a-4bfc-a92d-849607fd541" ]
 }
 
-def magic_cypher():
-    # # Add uniqueness constraints.
-    # # graph.run("CREATE CONSTRAINT ON (q:Question) ASSERT q.id IS UNIQUE;")
-    with open('data/mocks/CQC_data.json') as g:
-        data = json.load(g)
-
-    query = """
-    WITH $json as data
-    UNWIND data as q
-
-    MERGE (location:CQCLocation {id:q.loc.locationId}) ON CREATE
-    SET location.name = q.loc.name, location.web_link = q.loc.website, location.bed_count = q.loc.numberOfBeds, 
-    location.registrationStatus = q.loc.registrationStatus, location.registrationDate = q.loc.registrationDate, location.deregistrationDate = q.loc.deregistrationDate, location.onspdLongitude = q.loc.onspdLongitude, location.onspdLatitude = q.loc.onspdLatitude
-
-    MERGE (owner:PROVIDER {id:q.loc.providerId}) ON CREATE SET owner.postalAddressCounty = q.loc.postalAddressCounty
-    MERGE (owner)-[:Provides]->(location)
-
-    FOREACH (name IN q.loc.specialisms | MERGE (tag:specialism {name:name.name})  MERGE (location)-[:Specilises_In]->(tag)) 
-    FOREACH (a IN q.loc.reports | MERGE (location)<-[:REPORTS]-(answer:REPORT {id:a.linkId}) ON CREATE SET answer.reportDate = a.reportDate, answer.reportUri= a.reportUri, answer.firstVisitDate = a.firstVisitDate)
-
-    FOREACH (serviceType IN q.loc.gacServiceTypes |
-     MERGE (gacServiceType:gacServiceTypes {name:serviceType.name})
-     MERGE (location)-[:Supplies_gacServiceType]->(gacServiceType) )
-    """
-    graph.run(query,json=data)
 
 def register_client(client):
     print(f"Start registration for CLIENT : {client}")
@@ -69,5 +57,20 @@ def register_client(client):
     graph.run(query,json=client)
     print(f"Complete graph execution for client {client}")
 
-
 register_client(external_data)
+
+
+def get_client(ref):
+    '''To retrieve the profile of a client by id.'''
+    print(f"Start retrieval of CLIENT: {ref}")
+    query = """
+    match (client:CLIENT) where client.id = $ref return client as clt
+    """
+
+    print(f"Start graph execution for retrieving client {ref}")
+    result = graph.run(query,ref=ref)
+    print(f"Complete graph execution for client {ref}")
+    elem = result.single()
+    print(f"result: Name = {elem['clt']['name']},\n ID = {elem['clt']['id']}")
+
+client = get_client("7f644301-e3f1-4752-90d5-99fbfad91ab4")
